1c9f3a2234e42b158242e55296e5d0f614f3e4d7
Faisal K Barakat <107439864+FaisalKB@users.noreply.github.com>
2023-01-03 14:29:41 -0600

    Added comments

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("This will print to the console!");
+class Main { //This is the main class, which is sort of like a file. It holds all the relevant code
+  public static void main(String[] args) { //These are methods. They work like functions. This is the main method.
+    System.out.println("This will print to the console!"); //This is a print statement. It will print whatever is within the () to the console.
     System.out.println("Hello World!");
   } 
 }
